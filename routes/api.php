a60a53a40791f14c3ff696156029906520af8ed5
Gustavo Ramírez <gustavo81988@gmail.com>
2018-08-17 11:30:45 -0500

    laravel backend

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/quote', ['uses' => 'QuoteController@postQuote']);
+Route::get('/quotes', ['uses' => 'QuoteController@getQuotes']);
+Route::put('/quote/{id}', ['uses' => 'QuoteController@putQuote']);
+Route::delete('/quote/{id}', ['uses' => 'QuoteController@deleteQuote']);
